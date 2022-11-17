51de648ecc1f7d76bfc0124370f413c352927746
Don Tran <donqt15@gmail.com>
2022-11-16 17:20:43 -0800

    yo hotfix

--- Optix Group 4 Code/src/main/java/frc/robot/subsystems/Intake.java
+++ Optix Group 4 Code/src/main/java/frc/robot/subsystems/Intake.java
@@ -3,17 +3,32 @@ import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem {
-    Comressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+public class Intake extends SubsystemBase {
+    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+    DoubleSolenoid doubleSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.forward1, Constants.Intake.backward1);
+    DoubleSolenoid doubleSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.forward1, Constants.Intake.backward1);
+
+    public Intake() {
+        doubleSolenoid1.set(Value.kOff);
+        doubleSolenoid1.set(Value.kOff);
+    }
+
+    public void goForward () {
+        doubleSolenoid1.set(Value.kForward);
+        doubleSolenoid2.set(Value.kForward);
+    }
+    public void goBackward () {
+        doubleSolenoid1.set(Value.kReverse);
+        doubleSolenoid2.set(Value.kReverse);
+    }
     @Override
     public void periodic() {
         CompressorOn();
@@ -25,6 +40,8 @@ public class IntakeSubsystem {
     public void CompressorOff() {
         pcmCompressor.disable();
     }
+
+
     // Neo Motor Can Spark Max
     CANSparkMax neoMotorCanSparkMax = new CANSparkMax(0, MotorType.kBrushless);
 
