65f7f32d537ee1bdb548e8ba274a0e0113c2c5a5
will-w-cheng <will.wi.cheng@gmail.com>
2022-11-16 16:17:57 -0800

    compressor change

--- Optix Group 4 Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Optix Group 4 Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -8,5 +8,15 @@ import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class IntakeSubsystem {
-    
+    DoubleSolenoid DoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+    DoubleSolenoid DoublePH = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, 4, 5);
+    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
+    Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
+    public Pneumatics () {
+        pitchSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.PNEUMATICS_SOLENOID_DEPLOY, Constants.PNEUMATICS_SOLENOID_RETRACT);
+    }
+
+    public void CompressorOn() {
+
+    }
 }
