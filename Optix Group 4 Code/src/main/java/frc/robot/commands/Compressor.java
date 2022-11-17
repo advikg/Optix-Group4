1fffc1d4b0f03d17763092d79daefa6b43ebf5d2
will-w-cheng <will.wi.cheng@gmail.com>
2022-11-16 17:20:34 -0800

    Compressor command

--- Optix Group 4 Code/src/main/java/frc/robot/commands/Compressor.java
+++ Optix Group 4 Code/src/main/java/frc/robot/commands/Compressor.java
@@ -4,28 +4,44 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.*;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
+/** An example command that uses an example intake. */
+public class Compressor extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final Intake intake;
 
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new Compressor.
    *
-   * @param subsystem The subsystem used by this command.
+   * @param intake The intake used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public Compressor(Intake intake) {
+    this.intake = intake;
+    // Use addRequirements() here to declare intake dependencies.
+    addRequirements(intake);
   }
 
   // Called when the command is initially scheduled.
   @Override
-    public void periodic() {
-        CompressorOn();
-    }
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      intake.compressorOn();
+  }
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.compressorOff();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
--- Optix Group 4 Code/src/main/java/frc/robot/subsystems/Intake.java
+++ Optix Group 4 Code/src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import frc.robot.Constants;
@@ -11,7 +14,21 @@ import frc.robot.Constants;
 public class Intake extends SubsystemBase {
     DoubleSolenoid doubleSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.forward1, Constants.Intake.backward1 );
     DoubleSolenoid doubleSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.forward2, Constants.Intake.backward2 );
+    Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
 
+    public void compressorOn() {
+        pcmCompressor.enableDigital();
+    }
+
+    public void compressorOff() {
+        pcmCompressor.disable();
+    }
+    // Neo Motor Can Spark Max
+    CANSparkMax neoMotorCanSparkMax = new CANSparkMax(0, MotorType.kBrushless);
+
+    public void setNeo(double speed){
+        neoMotorCanSparkMax.set(speed);
+    }
     public Intake() {
         doubleSolenoid1.set(Value.kOff);
         doubleSolenoid2.set(Value.kOff);
