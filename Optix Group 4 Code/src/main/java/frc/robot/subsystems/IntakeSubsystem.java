34ac1a976c304bad8f48a8b475d87c52738d7867
Don Tran <donqt15@gmail.com>
2022-11-16 16:24:07 -0800

    canspark max and subsystembase

--- Optix Group 4 Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Optix Group 4 Code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems;
 
-public class IntakeSubsystem {
-    
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+    CANSparkMax neoMotorCanSparkMax = new CANSparkMax(0, MotorType.kBrushless);
+
 }
