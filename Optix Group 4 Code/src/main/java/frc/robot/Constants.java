07a4d1c9aec19f017b6e732d72d5f358fedd0c67
will-w-cheng <will.wi.cheng@gmail.com>
2022-11-16 16:10:45 -0800

    robot changes

--- Optix Group 4 Code/src/main/java/frc/robot/Constants.java
+++ Optix Group 4 Code/src/main/java/frc/robot/Constants.java
@@ -12,4 +12,8 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    DoubleSolenoid DoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+    DoubleSolenoid DoublePH = new DoubleSolenoid(9, PneumaticsModuleType.REVPH, 4, 5);
+}
