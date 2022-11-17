e5dd312e97a404bc4d4a67080b60033412ac0b11
YLu-1258 <maodou1258@gmail.com>
2022-11-16 17:11:05 -0800

    don's code

--- Optix Group 4 Code/src/main/java/frc/robot/subsystems/Intake.java
+++ Optix Group 4 Code/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,27 +1,33 @@
 package frc.robot.subsystems;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.subsystems;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import frc.robot.Constants;
-
-
-public class Intake extends SubsystemBase {
-    DoubleSolenoid doubleSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.forward1, Constants.Intake.backward1 );
-    DoubleSolenoid doubleSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Intake.forward2, Constants.Intake.backward2 );
-
-    public Intake() {
-        doubleSolenoid1.set(Value.kOff);
-        doubleSolenoid2.set(Value.kOff);
+public class IntakeSubsystem {
+    @Override
+    public void periodic() {
+        CompressorOn();
     }
-    public void goForward () {
-        doubleSolenoid1.set(Value.kForward);
-        doubleSolenoid2.set(Value.kForward);
+    public void CompressorOn() {
+        pcmCompressor.enableDigital();
     }
-    public void goBackward () {
-        doubleSolenoid1.set(Value.kReverse);
-        doubleSolenoid2.set(Value.kReverse);
+
+    public void CompressorOff() {
+        pcmCompressor.disable();
+    }
+    // Neo Motor Can Spark Max
+    CANSparkMax neoMotorCanSparkMax = new CANSparkMax(0, MotorType.kBrushless);
+
+    public void setNeo(double speed){
+        neoMotorCanSparkMax.set(speed);
     }
 }
